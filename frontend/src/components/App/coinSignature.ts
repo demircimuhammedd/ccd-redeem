480bf5c9ccd6168be0c7043eb7bd9153c6849e8f
demircimuhammedd <demirci.muhammedd@gmail.com>
2023-10-03 17:28:41 +0200

    fix signed message encoding

--- frontend/src/components/App/coinSignature.ts
+++ frontend/src/components/App/coinSignature.ts
@@ -1,6 +1,7 @@
 import nacl from "tweetnacl";
 import {Buffer} from 'buffer';
 import {base58_to_binary} from "base58-js"
+import {decode} from "wif"
 
 // This function computes the signature of CoinSecret on accountAddr
 function signAccount(CoinSeeed: string, accountAddr: string) {
@@ -11,12 +12,12 @@ function signAccount(CoinSeeed: string, accountAddr: string) {
             throw new Error("CoinSecret must be 32 bytes long.");
         }
         const keys = nacl.sign.keyPair.fromSeed(seed)
-        // Decode account address and sign it with key
-        const message = Buffer.from(accountAddr, 'hex');
+        // Decode account address and sign it with key (this requires base58check decoding)
+        const message = decode(accountAddr).privateKey;
         const signature = nacl.sign.detached(message,keys.secretKey)
         const hexSignature = Buffer.from(signature).toString('hex')
         const hexPubKey = Buffer.from(keys.publicKey).toString('hex')
-        return {message: accountAddr, pubkey: hexPubKey, signature: hexSignature}
+        return {message: message, pubkey: hexPubKey, signature: hexSignature}
     } catch(error) {
         return {e: error}
     }
--- smart-contract/tests/tests.rs
+++ smart-contract/tests/tests.rs
@@ -1,6 +1,9 @@
+use std::str::FromStr;
+
 use ccd_redeem::*;
 use concordium_smart_contract_testing::*;
 use concordium_std::{PublicKeyEd25519, SignatureEd25519};
+use hex::FromHex;
 
 const ACCOUNT_0: AccountAddress = AccountAddress([0; 32]);
 const ACC_INITIAL_BALANCE_0: Amount = Amount::from_ccd(200);
@@ -123,9 +126,78 @@ fn test_redeem() {
         result
             .coins
             .get(0)
-            .expect("Fisrt element expected to exist")
+            .expect("First element expected to exist")
             .1
             .is_redeemed,
         "The coin is expected to be redeemed"
     )
+}
+
+#[test]
+fn test_encoding() {
+    //Initialize contract with public key from 'generator.py'
+    let buffer = <[u8; 32]>::from_hex("0e74d2be36734c232e527b2ecc8d981ec898979f860359220274acf9c6def8f9").expect("Hex decoding pk should work");
+    let pubkey = PublicKeyEd25519(buffer);
+    //Initialize redeeming account from base58 string
+    let account_str = "4r81HqikiXBfwxjNJKJAWdw6an2jq4aGSZZAy8fM3fQ9a7x9mH";
+    let account_addr = AccountAddress::from_str(account_str).expect("Can decode account from base58");
+    println!("{:?}", account_addr.0);
+    //Initialize signature on account address from hex signature as produced by dapp
+    let buffer = <[u8; 64]>::from_hex("f7cef8a2afcc2b9ab10da90289610ecbfd4bd95043990145120e3c4b47a3b9a0e0d25fa686869beb1da504aa5a2dc7573fd0a5bf7e67fd6414e33614c8518703").expect("Hex decoding sig should work");
+    let signature = SignatureEd25519(buffer);
+
+
+    // Setup chain and module
+    let mut chain = Chain::new();
+    let account_admin = Account::new(ACCOUNT_0, ACC_INITIAL_BALANCE_0);
+    let account_other = Account::new(account_addr, ACC_INITIAL_BALANCE_1);
+    chain.create_account(account_admin);
+    chain.create_account(account_other);
+    let module =
+        module_load_v1("target/concordium/wasm32-unknown-unknown/release/ccd_redeem.wasm.v1")
+            .expect("Module is valid and exists");
+    let deployment = chain
+        .module_deploy_v1(Signer::with_one_key(), ACCOUNT_0, module)
+        .expect("Deploying valid module should succeed");
+
+    // Initialize contract with a coin for public key
+    let coins = vec![(pubkey, AMOUNT)];
+    let param_bytes = OwnedParameter::from_serial(&InitParam { coins })
+        .expect("Parameters should be serialized successfully");
+    let init_info = chain.contract_init(
+        Signer::with_one_key(),
+        ACCOUNT_0,
+        Energy::from(10000),
+        InitContractPayload {
+            mod_ref: deployment.module_reference,
+            init_name: OwnedContractName::new_unchecked("init_ccd_redeem".to_string()),
+            param: param_bytes,
+            amount: INITIAL_CONTRACT_BALANCE,
+        },
+    ).expect("Initialization should always succeed");
+
+    // Craft redeem call
+    let param = OwnedParameter::from_serial(&RedeemParam {
+        public_key: pubkey,
+        account: account_addr,
+        signature: signature,
+    })
+    .expect("Parameters should be serialized successfully");
+
+    chain
+        .contract_update(
+            Signer::with_one_key(),
+            account_addr,
+            Address::Account(account_addr),
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: init_info.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked("ccd_redeem.redeem".to_string()),
+                message: param,
+            },
+        )
+        .expect("Contract call succeeds");
+
+    
 }
